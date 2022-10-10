3add414acfcd01a59f22858e839dc66317cd8b40
SIDI MAMMAR MOHAMED AMEZIANE <rylessd@gmail.com>
2022-10-10 16:17:39 +0200

    Dérniére partie

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,6 +3,6 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name){
-     return new ConsoleLogger();
+        return new ContextualLogger(name, new FilteredLogger(new ConsoleLogger(), message->message.contains("Simulation")));
 }
 }
